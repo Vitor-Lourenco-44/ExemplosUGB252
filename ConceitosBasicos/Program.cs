6d2aa309b534008ac34746d2a3a82374c32c1317
Vitor-Lourenco-44 <pereias2@hotmail.com>
2025-08-12 20:37:59 -0300

    Código alterado para entrada de nome e salário pelo usuário, imprimindo os dados ao final.

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,10 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-int idade = 25;
-
-Console.WriteLine("A idade é" + idade);
-if (idade >= 18)
-    Console.WriteLine("Você pode tirar habilitação");
-
-else
-    Console.WriteLine("Você não pode tirar habilitação");
-
+﻿Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Informe seu salário: ");
+decimal salario = decimal.Parse( Console.ReadLine());
+Console.WriteLine("O seu salário é R$" + salario);
+Console.WriteLine("O seu nome é " + nome);
