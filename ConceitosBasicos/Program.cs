d9eeb97b4513dbeeea8c73636461b5bcbbf710a0
Vitor-Lourenco-44 <pereias2@hotmail.com>
2025-08-12 19:51:50 -0300

    Acrescentado o Else na programação e removida a divisão de blocos.

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -3,7 +3,8 @@ int idade = 25;
 
 Console.WriteLine("A idade é" + idade);
 if (idade >= 18)
-{
     Console.WriteLine("Você pode tirar habilitação");
-}
+
+else
+    Console.WriteLine("Você não pode tirar habilitação");
 
