b86fa57075e9377b4b1963104d1266b20a8b860d
Vitor-Lourenco-44 <pereias2@hotmail.com>
2025-08-12 21:13:19 -0300

    Uma parte do exercício realizada

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -5,9 +5,25 @@ decimal salario = decimal.Parse( Console.ReadLine());
 Console.WriteLine("O seu salário é R$" + salario);
 Console.WriteLine("O seu nome é " + nome);
 
+decimal calculo1 = salario * 0.0075m;
+
+{
+    if (salario <= 2259.20m)
+        Console.WriteLine("Você terá isenção de IRPF, parabéns!");
+}
+
+{
+
+    if (salario >= 2259.21m && <= 2826.65m)
+        Console.WriteLine("Você deverá pagar R${0} de IRPF com base no seu salário, ou seja, 7,5%", calculo1);
+}
+// if (salario >= 2826.66 && <= 3751.05)
+
+
+
 // Informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF.
 
-// Usar "Você terá um desconto de X em IRPF"
+// Usar "Você terá um desconto de X em IRPF" R$ 2.259,20
 
 // Usando o if
 
