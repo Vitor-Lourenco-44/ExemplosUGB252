bc0e4729b135d89e5d7529a6b580445d6189e8a2
Vitor-Lourenco-44 <pereias2@hotmail.com>
2025-08-19 20:08:52 -0300

    Bloco try catch

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,24 +1,37 @@
-﻿Console.WriteLine("Informe seu nome: ");
-string nome = Console.ReadLine();
-Console.WriteLine("Informe seu salário: ");
-decimal salario = decimal.Parse( Console.ReadLine());
-Console.WriteLine("O seu salário é R$" + salario);
-Console.WriteLine("O seu nome é " + nome);
+﻿try
+{
+    Console.WriteLine("Informe seu nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Informe seu salário: ");
+    decimal salario = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("O seu salário é R$" + salario);
+    Console.WriteLine("O seu nome é " + nome);
 
-decimal irpfDevido;
+    decimal irpfDevido;
 
-if (salario <= 2259.20m)
-    irpfDevido = 0;
-else if (salario <= 2826.65m)
-    irpfDevido = salario * 0.075m - 169.21m;
-else if (salario <= 3751.65m)
-    irpfDevido = salario * 0.15m - 381.44m;
-else if (salario <= 4664.68m)
-    irpfDevido = salario * 0.225m - 662.77m;
-else
-    irpfDevido = salario * 0.275m - 896m;
+    if (salario <= 2259.20m)
+        irpfDevido = 0;
+    else if (salario <= 2826.65m)
+        irpfDevido = salario * 0.075m - 169.21m;
+    else if (salario <= 3751.65m)
+        irpfDevido = salario * 0.15m - 381.44m;
+    else if (salario <= 4664.68m)
+        irpfDevido = salario * 0.225m - 662.77m;
+    else
+        irpfDevido = salario * 0.275m - 896m;
 
-Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é de {irpfDevido}, então o salário com desconto de IRPF será de {salario - irpfDevido}");
+    Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é de {irpfDevido}, então o salário com desconto de IRPF será de {salario - irpfDevido}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("O saláiro deve ser informado em formato numérico");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Ocorreu um erro. Contate o suporte técnico.");
+    Console.WriteLine(e.StackTrace);
+}
+
 
 
 
