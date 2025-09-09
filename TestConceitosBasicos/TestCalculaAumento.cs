e33da91c0ef38a8f0b0b2105ada03df227c34562
Vitor-Lourenco-44 <pereias2@hotmail.com>
2025-09-09 19:51:13 -0300

    Atualização do TestSomaDoisNumeros

--- TestConceitosBasicos/TestCalculaAumento.cs
+++ TestConceitosBasicos/TestCalculaAumento.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConceitosBasicos.uteis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace TestConceitosBasicos
 {
-    internal class TestCalculaAumento
+    [TestClass]
+    public class TesteCalculaAumento
     {
+        [TestMethod]
+        public void TesteCalcular()
+        {
+            //cenario
+            decimal salario = 1000;
+            float percentualAumento = 10;
+            decimal valorAumento;
+            CalculaAumento calculaAumento = new CalculaAumento();
+
+            //ação
+            valorAumento = calculaAumento.Calcular(salario, percentualAumento);
+
+            //verificação
+            Assert.AreEqual(100, valorAumento);
+        }
     }
 }
